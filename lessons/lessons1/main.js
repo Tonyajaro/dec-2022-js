428b0281a7929038c663f2d72b4315da0ceaffd5
Tonyajaro <Tonya.toropchina@gmail.com>
2023-01-31 23:00:58 +0100

    losson1

---
+++ lessons/lessons1/main.js
@@ -0,0 +1,51 @@
+let strText = 'Hello';
+console.log(strText);
+
+let str = 'owu';
+console.log(str);
+
+let strCom = 'com';
+console.log(strCom)
+
+let strUa = 'ua';
+console.log(strUa)
+
+let num = 1;
+console.log(num);
+
+let num1 = 10;
+console.log(num1);
+
+let num2 = -999;
+console.log(num2);
+
+let num3 = 123;
+console.log(num3);
+
+let num4 = 3.14;
+console.log(num4);
+
+let num5 = 2.7;
+console.log(num5);
+
+let num6 = 16;
+console.log(num6);
+
+let a1 = true;
+console.log(a1);
+
+let b1 = false;
+console.log(b1);
+
+let firstName = 'Antonina';
+let middleNane = 'Stepanivna';
+let lastName = 'Toropchina';
+
+console.log(`Person - ${firstName} ${middleNane} ${lastName}`);
+
+let a = 100;
+console.log(typeof a);
+let b = `100`;
+console.log(typeof b);
+let c = true
+console.log(typeof c);
